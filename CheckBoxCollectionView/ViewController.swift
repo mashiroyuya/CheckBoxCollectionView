f44b2d0462835530f317be9e936ab6a254646f9b
山城悠 <yuyamashiro@yuyamashiro-no-macbook-pro.local>
2016-02-29 22:44:45 +0900

    Initial Commit

---
+++ CheckBoxCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckBoxCollectionView
+//
+//  Created by 山城悠 on 2016/02/29.
+//  Copyright © 2016年 山城悠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
